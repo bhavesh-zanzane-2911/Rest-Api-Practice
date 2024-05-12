3bde5c387b816ba638dac823f58c8c1e7e2fce6f
Bhavesh Zanzane <85382125+bhavesh-zanzane-2911@users.noreply.github.com>
2024-05-12 17:33:35 +0530

    Update StudentJpaRepository.java

--- src/main/java/com/bhavesh/restpractice/newpractise/repository/StudentJpaRepository.java
+++ src/main/java/com/bhavesh/restpractice/newpractise/repository/StudentJpaRepository.java
@@ -2,9 +2,9 @@ package com.bhavesh.restpractice.newpractise.repository;
 
 import com.bhavesh.restpractice.newpractise.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface StudentJpaRepository extends JpaRepository<Student, Integer> {
 
 }
