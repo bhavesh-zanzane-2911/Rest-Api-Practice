cc9e24fc3297be198d23ea12dd7deb534af240e2
bhavesh-zanane-2911 <bhaveshzanzane2911@outlook.com>
2025-06-08 20:07:14 +0530

    Added Deployment configs

--- src/main/java/com/bhavesh/restpractice/newpractise/NewPractiseApplication.java
+++ src/main/java/com/bhavesh/restpractice/newpractise/NewPractiseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewpractiseApplication {
+public class NewPractiseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NewpractiseApplication.class, args);
+        SpringApplication.run(NewPractiseApplication.class, args);
     }
 
 }
