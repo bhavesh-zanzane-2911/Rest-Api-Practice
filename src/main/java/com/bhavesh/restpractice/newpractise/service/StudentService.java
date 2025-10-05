a58cad2952df621ebb7fb5f496d1845e85de7b00
Bhavesh Zanzane <bhaveshzanzane2911@gmail.com>
2025-10-05 21:08:14 +0530

    Removed Unrequired methods

--- src/main/java/com/bhavesh/restpractice/newpractise/service/StudentService.java
+++ src/main/java/com/bhavesh/restpractice/newpractise/service/StudentService.java
@@ -9,69 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class StudentService {
-    static List<Student> studentList = new ArrayList<>();
 
     private final StudentRepository studentRepository;
-
-    static {
-        studentList.add(new Student(1, "Adesh", "Pune"));
-        studentList.add(new Student(2, "Bhavesh", "Baramati"));
-        studentList.add(new Student(3, "Pooja", "Indapur"));
-    }
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-    public Student findById(int rollNo) {
-        for (Student student : studentList) {
-            if (student.getRollNo() == rollNo) {
-                return student;
-            }
-        }
-
-        return null;
-    }
-
-    public Student getStudentByRollNo(int rollNo){
+    public Student getStudentByRollNo(int rollNo) {
         return studentRepository
                 .findById(rollNo)
-                .orElseThrow(()->new StudentNotFoundException("Student with RollNo " + rollNo + " Not Found"));
-    }
-
-    public List<Student> findAllStudents() {
-        return studentList;
-    }
-
-    public void deleteById(int id) {
-        Iterator<Student> iterator = studentList.iterator();
-        while (iterator.hasNext()) {
-            Student next = iterator.next();
-            if (next.getRollNo() == id) {
-                iterator.remove();
-            }
-        }
-    }
-
-    public void updateStudentDetails(Student student) {
-
-        for (Student s : studentList) {
-            if (s.getRollNo() == student.getRollNo()) {
-                s.setCity(student.getCity());
-                s.setName(student.getName());
-            }
-        }
-
-    }
-
-    public void addStudent(Student student) {
-        studentList.add(student);
+                .orElseThrow(() -> new StudentNotFoundException("Student with RollNo " + rollNo + " Not Found"));
     }
 
     public List<Student> getAllStudents() {
@@ -79,11 +31,10 @@ public class StudentService {
     }
 
     public void deleteStudentByRollNo(int rollNo) {
-        if(!studentRepository.existsById(rollNo)) {
+        if (!studentRepository.existsById(rollNo)) {
             throw new StudentNotFoundException("Student with RollNo " + rollNo + " Not Found");
         }
         studentRepository.deleteById(rollNo);
-
     }
 
     public ResponseEntity<Object> saveStudent(@Valid Student student) {
@@ -93,7 +44,7 @@ public class StudentService {
     }
 
     public void updateStudent(Student student) {
-        if(!studentRepository.existsById(student.getRollNo())) {
+        if (!studentRepository.existsById(student.getRollNo())) {
             throw new StudentNotFoundException("Student with RollNo " + student.getRollNo() + " Not Found");
         }
         studentRepository.save(student);
